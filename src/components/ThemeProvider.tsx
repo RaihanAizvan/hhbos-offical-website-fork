5a2d02a83e4e0910555f1f4ac2956f7196383e71
Mohammed Raihan <aizvanr@gmail.com>
2026-01-06 20:24:46 +0530

    feat: add ThemeProvider

---
+++ src/components/ThemeProvider.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
+
+/**
+ * App-wide theme provider.
+ *
+ * We use Tailwind's `darkMode: ["class"]`, so next-themes must control a class on the root element.
+ */
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="dark" {...props}>
+      {children}
+    </NextThemesProvider>
+  );
+}
