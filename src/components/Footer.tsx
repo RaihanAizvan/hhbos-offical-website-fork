9b25ae9d24bc7e2e2a4db57e5f04752a99f06f5e
Mohammed Raihan <aizvanr@gmail.com>
2026-01-30 14:57:33 +0530

    content(footer): copyright 2026 -> 2020

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear();
+  // const currentYear = new Date().getFullYear();
+  const currentYear = 2020;
 
   const footerLinks = {
     company: [
